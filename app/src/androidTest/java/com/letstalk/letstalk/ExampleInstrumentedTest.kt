ed1545847dd66e34cad881acbf508150c03b9a88
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-09-13 07:45:43 +0700

    Update testing

--- app/src/androidTest/java/com/letstalk/letstalk/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/letstalk/letstalk/ExampleInstrumentedTest.kt
@@ -1,14 +1,9 @@
 package com.letstalk.letstalk
 
-import android.content.Context
-
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
-import org.junit.Assert.assertEquals
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,8 +15,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-
+        val appContext = androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.letstalk.letstalk", appContext.packageName)
     }
 }
